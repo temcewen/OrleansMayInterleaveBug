4f01b51e1577dde2c777dad666bb53e92336da52
temcewen <tmcewen.developer@gmail.com>
2024-10-07 12:01:50 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using HelloWorld;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Orleans.Configuration;
+using Orleans.Runtime;
+
+var host = new HostBuilder()
+    .UseOrleans(builder =>
+    {
+        builder.UseLocalhostClustering();
+    })
+    .Build();
+
+await host.StartAsync();
+
+
+var grainFactory = host.Services.GetRequiredService<IGrainFactory>();
+var test1Grain1 = grainFactory.GetGrain<ISimpleGrain>("1");
+await test1Grain1.Tell();
+
+await host.StopAsync();
---
+++ SimpleGrain.cs
@@ -0,0 +1,27 @@
+﻿using Orleans.Concurrency;
+using Orleans.Runtime;
+using Orleans.Serialization.Invocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld;
+
+[MayInterleave(nameof(ArgHasInterleaveAttribute))]
+public class SimpleGrain : Grain, ISimpleGrain
+{
+
+    public static bool ArgHasInterleaveAttribute(IInvokable req)
+    {
+        Console.WriteLine("May interleave call.");
+        return true;
+    }
+
+    public Task Tell()
+    {
+        Console.WriteLine("Grain method call.");
+        return Task.CompletedTask;
+    }
+}
